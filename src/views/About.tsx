0d2c0cd8ff44ba40fee8ddbe5a0ab031ea2aab45
David Feng <davidxifeng@gmail.com>
2023-12-17 09:27:58 +0900

    tailwind typography

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,5 +1,10 @@
 export function ReactRoot() {
     return <>
         <button className="btn">hello</button>
+        <article className="prose">
+            <h1>title</h1>
+            <p>tailwind & daisy ui is great</p>
+            <button >hello 2</button>
+        </article>
     </>
 }
